68ab033d39e249c62934e0501febcff208207665
Manu Anish <manusanish@gmail.com>
2022-11-06 11:29:44 +0530

    Update web meta

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Body, Header, Meta } from "@components/web";
 export default function Home() {
   return (
     <div>
-      <Meta />
+      <Meta title="Celer" description="🚀 Instantly share beautiful notes, latex, markdown, and more!"/>
       <Header />
       <Body>Hello World!</Body>
     </div>
