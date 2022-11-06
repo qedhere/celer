a0b428617b9cb75507cfb0a81630d0cffeaae3da
Manu Anish <manusanish@gmail.com>
2022-11-06 19:47:44 +0530

    Working on Hero Page

--- components/Body/Body.js
+++ components/Body/Body.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Body(props) {
   return (
     <div className="flex justify-center w-full pl-4 pr-4">
-      <div className="max-w-[900px] grow">{props.children}</div>
+      <div className="max-w-[1000px] grow">{props.children}</div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import * as React from "react";
+import styles from "@styles/Home.module.css";
 import { Body, Header, Meta } from "@components/web";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
-  const [heroText, setHeroText] = React.useState("heroText1");
+  const [heroText, setHeroText] = React.useState(styles.heroText1);
+  const [buttonBackground, setButtonBackground] = React.useState(styles.bg1);
+  const [blink, isBlink] = React.useState("");
   return (
     <div>
       <Meta
@@ -13,7 +16,7 @@ export default function Home() {
       <Header />
       <Body>
         <div className="mt-[128px] sm:mt-[256px]">
-          <div className="text-5xl font-bold tracking-tighter pb-2 flex items-center gap-2 flex flex-wrap justify-center duration-200">
+          <div className="text-6xl font-bold tracking-tighter pb-2 flex items-center gap-2 flex flex-wrap justify-center duration-200">
             Instantly share stunning{" "}
             <div className={heroText}>
               <TypeAnimation
@@ -21,24 +24,45 @@ export default function Home() {
                   "notes.",
                   2000,
                   "",
-                  () => setHeroText("heroText2"),
+                  () => {
+                    setHeroText(styles.heroText2);
+                    isBlink(styles.blink);
+                    setButtonBackground(styles.bg2);
+                  },
                   "latex.",
                   2000,
                   "",
-                  () => setHeroText("heroText3"),
+                  () => {
+                    setHeroText(styles.heroText3);
+                    isBlink(styles.blink);
+                    setButtonBackground(styles.bg3);
+                  },
                   "markdown.",
                   2000,
                   "",
-                  () => setHeroText("heroText1"),
+                  () => {
+                    setHeroText(styles.heroText1);
+                    isBlink(styles.blink);
+                    setButtonBackground(styles.bg1);
+                  },
                 ]}
                 wrapper="span"
                 cursor={false}
                 repeat={Infinity}
               />
             </div>
+            <div className={`${styles.blink} ${heroText}`}>
+              I
+            </div>
           </div>
         </div>
-        <div className="flex justify-center mt-[256px]"><button className="pl-4 pr-4">ok</button></div>
+        <div className="flex justify-center mt-[256px]">
+          <button className={`rounded-lg w-[196px] h-[48px] flex justify-center items-center pb-[1px]`}>
+            <div className="flex justify-center items-center p-[1px] bg-black w-full h-full rounded-lg">
+            <div className="bg-white w-full h-full rounded-lg flex justify-center items-center">Get Started</div>
+            </div>
+          </button>
+        </div>
       </Body>
     </div>
   );
