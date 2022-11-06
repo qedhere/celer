a6802a0daa9d6023e32a086a7c17114338122c24
Manu Anish <manusanish@gmail.com>
2022-11-06 15:19:20 +0530

    Finish animated text

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import * as React from "react";
 import { Body, Header, Meta } from "@components/web";
-import { TypeAnimation } from 'react-type-animation';
-import { styles } from "@styles/Home.module.css"
-
+import { TypeAnimation } from "react-type-animation";
 
 export default function Home() {
+  const [heroText, setHeroText] = React.useState("heroText1");
   return (
     <div>
       <Meta
@@ -13,22 +13,26 @@ export default function Home() {
       <Header />
       <Body>
         <div className="mt-[128px] sm:mt-[256px]">
-          <div className="text-5xl font-bold tracking-tighter pb-2 flex items-center gap-2 flex flex-wrap justify-center">
+          <div className="text-5xl font-bold tracking-tighter pb-2 flex items-center gap-2 flex flex-wrap justify-center duration-200">
             Instantly share stunning{" "}
-            <TypeAnimation
-              sequence={[
-                'notes.', 
-                2000, 
-                'latex.', 
-                2000, 
-                'markdown.',
-                2000
-              ]}
-              wrapper="span"
-              cursor={true}
-              repeat={Infinity}
-              
-            />
+            <div className={heroText}>
+              <TypeAnimation
+                sequence={[
+                  "notes.",
+                  3000,
+                  () => setHeroText("heroText2"),
+                  "latex.",
+                  3000,
+                  () => setHeroText("heroText3"),
+                  "markdown.",
+                  3000,
+                  () => setHeroText("heroText1"),
+                ]}
+                wrapper="span"
+                cursor={false}
+                repeat={Infinity}
+              />
+            </div>
           </div>
         </div>
       </Body>
