8980b84ac2734d9235664cd15195b6931e47c1a3
Manu Anish <manusanish@gmail.com>
2022-11-08 15:39:43 +0530

    Working on index.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import { Body, Header, Meta } from "@components/web";
 import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
 import { IoLogoVercel, IoLogoFirebase } from "react-icons/io5";
+import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
 import Image from "next/image";
+import { ArrowRightIcon, RepoIcon } from "@primer/octicons-react";
 
 export default function Home() {
   const [heroText, setHeroText] = React.useState(styles.heroText1);
@@ -116,8 +118,93 @@ export default function Home() {
             </Link>
           </button>
         </div>
+        <div className="">
+          <div className="flex items-center gap-4">
+            <div className="ml-[6px] flex items-center justify-center mb-4"><RepoIcon size={24} className="text-gray-500"/></div>
+          </div>
+          <div className="ml-4 h-[50px] bg-gradient-to-b from-transparent to-gray-300 w-[1px]"></div>
+          <div className="ml-4 h-[150px] bg-gradient-to-b from-gray-300 to-[#0070f3] w-[1px]"></div>
+          <div className="ml-4 h-[50px] bg-gradient-to-b from-[#0070f3] to-transparent w-[1px]"></div>
+          <div>
+            <div
+              className={`${styles.heroText1} font-semibold tracking-tighter text-lg mb-2 mt-4`}
+            >
+              Free
+            </div>
+            <div
+              className={`text-4xl ${styles.textGradient} font-bold tracking-tighter max-w-[420px] z-30`}
+              style={{textShadow: "20px 20px 100px #FFFFFF"}}
+            >
+              Because knowledge was meant to be free.
+            </div>
+            <div
+              className={`text-xl tracking-tight max-w-[420px] z-30 mt-4 text-gray-400`}
+              style={{textShadow: "20px 20px 100px #FFFFFF"}}
+            >
+              Unleash your creativity without limits.
+            </div>
+            <button className="bg-[#0070f315] pl-4 pr-4 pt-1 pb-1 rounded-full mt-5 text-[#0070f3] flex items-center gap-2 hover:bg-[#0070f325] duration-200">Learn more <ArrowRightIcon/></button>
+          </div>
+          <div className="flex justify-center relative w-full">
+            <div className="max-w-[500px] p-10 mt-10">
+              <Image
+                src="/equations/wave-equation.svg"
+                width={300}
+                height={300}
+                alt="Wave Equation"
+              />
+              <div className="font-bold tracking-tighter mt-10">Schrödinger equation</div>
+              <div className="text-sm max-w-[320px]">The Schrödinger equation governs the wave function of a quantum-mechanical system. </div>
+            </div>
+            <div className='absolute top-[-20px] left-[180px] opacity-20 blur-[5px]'>
+            <Image
+                src="/equations/field-equation.svg"
+                width={300}
+                height={300}
+                alt="Wave Equation"
+              />
+              <div className="font-bold tracking-tighter mt-10">Einstein field equations</div>
+              <div className="text-sm max-w-[320px]">The Einstein field equations relate the geometry of spacetime to the distribution of matter within it. </div>
+            </div>
+            <div className='absolute bottom-[0px] right-[100px] opacity-30 blur-[2px]'>
+            <Image
+                src="/equations/fourier-equation.svg"
+                width={300}
+                height={300}
+                alt="Wave Equation"
+              />
+              <div className="font-bold tracking-tighter mt-10">Fourier transform</div>
+              <div className="text-sm max-w-[320px]">A Fourier transform is a mathematical transform that decomposes functions into frequency components. </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`${styles.heroText2} font-bold tracking-tight text-xl`}
+            >
+              Shared
+            </div>
+          </div>
+          <div>
+            <div
+              className={`${styles.heroText3} font-bold tracking-tight text-xl`}
+            >
+              Accessible
+            </div>
+          </div>
+        </div>
+        <div className="flex mt-20">
+          <div className="w-[50px] xs:w-[75px] sm:w-[100px] bg-gradient-to-l from-gray-300 to-white pt-[1px] pb-[1px] flex justify-center items-center">
+            <div className="w-[50px] xs:w-[75px] sm:w-[100px] bg-gradient-to-l from-gray-100 to-white h-full"></div>
+          </div>
+          <div className="w-full h-[200px] bg-gray-100 border-t border-b border-gray-300 flex justify-center items-center gap-5 text-gray-400">
+            <IoLogoVercel size={32} /> <IoLogoFirebase size={32} />{" "}
+            <SiNextdotjs size={32} /> <SiTailwindcss size={32} />
+          </div>
+          <div className="w-[50px] xs:w-[75px] sm:w-[100px] bg-gradient-to-r from-gray-300 to-white pt-[1px] pb-[1px] flex justify-center items-center">
+            <div className="w-[50px] xs:w-[75px] sm:w-[100px] bg-gradient-to-r from-gray-100 to-white h-full"></div>
+          </div>
+        </div>
       </Body>
-      
       <div className="h-[64px] md:h-[100px] w-full"></div>
     </div>
   );
