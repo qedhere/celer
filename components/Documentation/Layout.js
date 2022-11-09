fdcb8326f405a5fcd37888e4e1b745e4319c524d
Manu Anish <manusanish@gmail.com>
2022-11-09 15:36:24 +0530

    Working on Docs...

--- components/Documentation/Layout.js
+++ components/Documentation/Layout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Header } from "@components/web";
+import { Divider } from "@geist-ui/core";
+import Tag from "./Tag"
 
 export default function Layout(props) {
   return (
@@ -7,6 +9,14 @@ export default function Layout(props) {
       <Header />
       <div className="md:pt-[256px] pt-[128px] w-full flex justify-center pl-4 pr-4">
         <article className="max-w-[1000px] prose lg:prose-lg prose-black dark:prose-invert grow">
+          <div className="flex flex-wrap gap-2">
+            {props.meta.tags.map((tag) => (
+              <Tag key={tag}>{tag}</Tag>
+            ))}
+          </div>
+          <div className="md:text-6xl font-semibold tracking-tighter mt-4 xs:text-5xl text-4xl">{props.meta.title}</div>
+          <div className="text-xl text-gray-500 mt-2 mb-5">{props.meta.description}</div>
+          <Divider/>
           {props.children}
           <div className="mt-[200px]"></div>
         </article>
---
+++ components/Documentation/Tag.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Tag(props) {
+  return (
+    <div className="rounded-full bg-[#0070f320] pl-4 pr-4 pt-1 pb-1 w-fit text-sm text-success-300">{props.children}</div>
+  )
+}
