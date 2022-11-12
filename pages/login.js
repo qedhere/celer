70970295f88d2f5c7131af639ba26a0ef9701af2
Manu Anish <manusanish@gmail.com>
2022-11-12 12:32:17 +0530

    Fix Data URI and add UID

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,9 @@ import * as React from "react";
 
 import { Header, Body } from "@components/web"
 import { useUser } from "@components/hooks"
+import { createAvatar } from '@dicebear/avatars';
+import * as style from '@dicebear/micah';
+import Image from "next/image";
 
 export default function Login(){
   const {user, loading} = useUser();
@@ -9,13 +12,23 @@ export default function Login(){
   React.useEffect(() => {
     console.log(user, loading);
   })
+
+  let svg = createAvatar(style, {
+    seed: 'iHxp',
+    dataUri: true
+  });
+
   return (
     <div>
       <Header/>
       <Body>
-        <div className="flex flex-col w-full mt-[256px]">
-          <div className="grow text-xl font-semibold tracking-tighter text-center">Your journey begins here.</div>
-          <div className="grow "></div>
+        <div className="w-full mt-[256px]">
+          <div className="text-xl font-semibold tracking-tighter text-center">Your journey begins here.</div>
+          <div className="flex justify-center w-full mt-20">
+            <div className="border rounded-full p-8 flex justify-center items-center">
+              <Image src={svg} width={100} height={100} alt="Avatar"/>
+            </div>
+          </div>
         </div>
       </Body>
     </div>
